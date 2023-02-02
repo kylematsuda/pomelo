0bb3090eb95c7b640d0bd76e09ecfa8c6fd06ace
Kyle Matsuda <kyle.yoshio.matsuda@gmail.com>
2023-02-02 16:31:42 -0700

    clean up placeholder things

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,1 @@
-use pomelo_lsp::LspBackend;
-use tower_lsp::{LspService, Server};
-
-#[tokio::main]
-async fn main() {
-    tracing_subscriber::fmt().init();
-
-    let (stdin, stdout) = (tokio::io::stdin(), tokio::io::stdout());
-    let (service, socket) = LspService::new(|client| LspBackend::new(client));
-    Server::new(stdin, stdout, socket).serve(service).await;
-}
+fn main() {}
