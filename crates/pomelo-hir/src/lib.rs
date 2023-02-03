841dbeec93dfd65df84cd73ce3beaf66e982873e
Kyle Matsuda <kyle.yoshio.matsuda@gmail.com>
2023-02-03 11:52:52 -0700

    add small docs for hir

--- crates/pomelo-hir/src/lib.rs
+++ crates/pomelo-hir/src/lib.rs
@@ -1,3 +1,6 @@
+//! High-level intermediate representation (HIR) for `pomelo`.
+//!
+//! This is very incomplete / changing a lot / poorly documented.
 pub mod arena;
 pub mod core;
 pub mod identifiers;
